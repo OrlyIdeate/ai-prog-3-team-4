efb998b89edd94e238dbdf6d0dd4708f4d78bc3d
OrlyIdeate <ueno.jin25@outlook.jp>
2024-11-25 19:30:12 +0900

    feat: テキストの言語を判定し、英語の場合は翻訳をスキップする機能を追加

--- services/translation.py
+++ services/translation.py
@@ -1,10 +1,17 @@
 from deepl import Translator
 from dotenv import load_dotenv
 import os
+import langdetect
 
 def translate(text):
     '''日本語から英語に翻訳'''
     load_dotenv('.env')
     translator = Translator(os.getenv('DEEPL_API_KEY'))
+    
+    # テキストが英語かどうかを判定
+    detected_lang = langdetect.detect(text)
+    if detected_lang == 'en':
+        return text
+    
     result = translator.translate_text(text, source_lang='JA', target_lang='EN-US')
     return result
