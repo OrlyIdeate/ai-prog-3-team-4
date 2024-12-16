3dfaadb2a3924c8571989fe3daaec2b7b295072e
OrlyIdeate <ueno.jin25@outlook.jp>
2024-12-16 17:55:29 +0900

    refactor: 不要なインポートを削除し、コードをクリーンアップ

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, request, send_file
-import openai
-import os
-from dotenv import load_dotenv
 import requests
 from io import BytesIO
 from services.openai_client import create_image
@@ -9,9 +6,6 @@ from services.translation import translate
 
 
 app = Flask(__name__)
-load_dotenv('.env')
-
-openai.api_key = os.getenv('OPENAI_API_KEY')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
